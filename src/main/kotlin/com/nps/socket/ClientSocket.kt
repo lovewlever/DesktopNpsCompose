8d136a737c25c5ada25c8e741eeb97ae72959ecd
LKPC <1044624802@qq.com>
2021-07-30 10:48:35 +0800

    socket

--- src/main/kotlin/com/nps/socket/ClientSocket.kt
+++ src/main/kotlin/com/nps/socket/ClientSocket.kt
@@ -1,5 +1,7 @@
 package com.nps.socket
 
+import androidx.compose.runtime.SideEffect
+import com.nps.common.NPCCommon
 import com.nps.common.ThreadPoolCommon
 import java.io.BufferedInputStream
 import java.io.FileOutputStream
@@ -16,6 +18,9 @@ class ClientSocket {
     private lateinit var printWriter: PrintWriter
 
     fun connect() {
+        SideEffect {
+            NPCCommon.startNpcClient("-server=8.140.170.239:8024 -vkey=nilgerw43tew0c8n -type=tcp -password=remote -target=127.0.0.1:2120 127.0.0.1:2121")
+        }
         ThreadPoolCommon.scheduled.execute {
             socket = Socket("127.0.0.1", 8025)
         }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -7,10 +7,6 @@ import com.nps.ui.TestSocketCompose
 fun main() = Window(
     //icon = ImageIO.read(File("nps.png"))
 ) {
-    SideEffect {
-        NPCCommon.startNpcClient("-server=8.140.170.239:8024 -vkey=nilgerw43tew0c8n -type=tcp -password=remote -target=127.0.0.1:2120 127.0.0.1:2121")
-    }
-
     MaterialTheme {
         TestSocketCompose()
         //FileDirectoryOrListCompose("/")
